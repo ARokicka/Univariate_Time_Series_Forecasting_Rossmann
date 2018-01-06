196502b51597e9db50191b78b90a12308df3a8cc
Kuba Winicjusz <kdemianowski@gmail.com>
2018-01-06 19:17:33 +0100

    import danych dziala

---
+++ import_data.R
@@ -0,0 +1,27 @@
+library(forecast)
+library(fpp2)
+library(xts)
+
+global_temp = read.csv("data/GlobalTemperatures.csv")
+global_temp$dt = as.Date(global_temp$dt, "%Y-%m-%d") 
+global_temp <- subset(global_temp, select = c(LandAverageTemperature) )
+str(global_temp)
+temp_ts <-  ts(global_temp, frequency = 12, start = c(1750,1), end =   c(2015,12))
+temp_zoo
+
+#tiff('test.tiff', units="in", width=40, height=10, res=600)
+
+
+plot.ts(temp_ts)  
+#dev.off()
+
+#global_temp = subset(global_temp, select = c(dt, LandAverageTemperature) )
+#global_temp.ts = ts(global_temp, frequency=12, start=c(1750,1) )
+#global_temp <- subset(global_temp, select = c(LandAverageTemperature) )
+# global_temp_ts = ts(global_temp, frequency=12, start=c(1750,1) )
+#plot(global_temp$LandAverageTemperature ~ global_temp$dt, xaxt = "n", type = "l")
+
+
+
+
+
