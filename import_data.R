e65a0fb8946cda67c53dc01be678b063cdec765d
Kuba Winicjusz <kdemianowski@gmail.com>
2018-01-06 19:29:32 +0100

    drobne zmiany

--- import_data.R
+++ import_data.R
@@ -1,17 +1,18 @@
 library(forecast)
 library(fpp2)
 library(xts)
+library('tseries')
+
 
 global_temp = read.csv("data/GlobalTemperatures.csv")
 global_temp$dt = as.Date(global_temp$dt, "%Y-%m-%d") 
 global_temp <- subset(global_temp, select = c(LandAverageTemperature) )
 str(global_temp)
 temp_ts <-  ts(global_temp, frequency = 12, start = c(1750,1), end =   c(2015,12))
-temp_zoo
+
+
 
 #tiff('test.tiff', units="in", width=40, height=10, res=600)
-
-
 plot.ts(temp_ts)  
 #dev.off()
 
