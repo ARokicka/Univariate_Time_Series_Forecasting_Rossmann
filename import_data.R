39673f67c2e3410e35eaa7d5acff100505cdfa82
kuba_dem <kdemianowski@gmail.com>
2018-04-11 20:26:33 +0200

    zapomnialem nacisnac ctrl s

--- import_data.R
+++ import_data.R
@@ -12,6 +12,7 @@ train_2m = read.csv("D:/Praca/Analityka retail/rossmann/train_2m.csv")
 prepare_data(test_2m)
 prepare_data(train_2m)
 
+
 test_ts <- zoo(test_aggregated$Sales, order.by=as.Date(as.character(test_aggregated$Date), format='%Y-%m-%d'))
 test_ts <- ts(test_ts, start=c(2015,07,23), frequency=7)
 
